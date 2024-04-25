09c643fe165e6d5638319db3b0c9dff074e0c35e
KPriyadh <priyadharshini.sf4630@gmail.com>
2024-04-26 03:24:50 +0530

    Addind Advance OOPS File

--- ABSTRACTION/PartialClassesAndMethods/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace PartialClassesAndMethods;
-class Program{
-    public static void Main(string[] args)
-    {
-        PersonalDetails person1 = new PersonalDetails();
-        
-    }
-}
--- ENCAPSULATION/Outside/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace Outside;
-class Program{
-    public static void Main(string[] args)
-    {
-        
-    }
-}
